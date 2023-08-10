172a7a4d0b175476981e6569dbd5ca86f976acd9
vi <56130150+vishrutiii@users.noreply.github.com>
2023-08-10 14:33:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,48 +1,23 @@
 //create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
-const bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
+var Post = require('../models/post');
+var User = require('../models/user');
+var auth = require('../middlewares/auth');
 
-//use body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-//set view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-//set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//set routes
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Index' });
-});
-
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
-});
-
-app.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact' });
-});
-
-app.post('/contact/send-message', (req, res) => {
-    const { author, sender, title, message } = req.body;
-
-    if (author && sender && title && message) {
-        res.render('contact', { isSent: true, title: 'Contact' });
-    } else {
-        res.render('contact', { isError: true, title: 'Contact' });
+//create comment
+router.post('/', auth.ensureAuthenticated, function (req, res, next) {
+    var comment = new Comment({
+        content: req.body.content,
+        post: req.body.post,
     }
-});
-
-app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
-});
-
-app.listen(port, () => {
-    console.log(`App is listening at http://localhost:${port}`);
+    );
+    comment.save(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        res.status(201).json(comment);
+    }   
+    );
 });
