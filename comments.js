96d5a0196e974c42c72ca9d22cbc572ff66b4c50
vi <56130150+vishrutiii@users.noreply.github.com>
2023-08-10 14:51:47 +0000

    Copilot Third commit

--- comments.js
+++ comments.js
@@ -1,44 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import path from 'path';
-
-//import express from 'express';
-//import bodyParser from 'body-parser'; 
-//import { MongoClient } from 'mongodb';
-//import path from 'path';
-
-//create web server 
+//Create web server
+const express = require('express');
 const app = express();
-
-//connect to mongodb
-MongoClient.connect('mongodb://localhost:27017/test', (err, db) => {
-  if (err) throw err;
-  console.log('Connected to MongoDB');
-
-  //use body parser to parse json data
-  app.use(bodyParser.json());
-
-  //serve static files from the dist directory
-  app.use(express.static(path.join(__dirname, '/dist')));
-
-  //get comments from mongodb
-  app.get('/api/comments', (req, res) => {
-    db.collection('comments').find({}).toArray((err, comments) => {
-      if (err) throw err;
-      res.json(comments);
+//Create a port
+const port = 3000;
+//Create a mongoose
+const mongoose = require('mongoose');
+//Create a schema
+const Schema = mongoose.Schema;
+//Connect to mongodb
+mongoose.connect('mongodb://localhost:27017/itc-ecommerce-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+//Create a model
+const Comment = mongoose.model('Comment', {
+    comment: String,
+    product_id: String,
+    user_id: String,
+    c_date: Date
+});
+//Create a route
+app.get('/comments', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        res.send(comments);
     });
-  });
-
-  //add new comment to mongodb
-  app.post('/api/comments', (req, res) => {
-    const newComment = {
-      text: req.body.text,
-      id: Date.now(),
-    };
-    db.collection('comments').insertOne(newComment, (err, result) => {
-        if (err) throw err;
-        res.json(result.ops[0]);
-        });
-    });
+});
+//Run the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
